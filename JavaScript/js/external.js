6efe8d765f1444e77a3682ae6c0ed57d48cd55a7
islamm88 <142352912+islamm88@users.noreply.github.com>
2026-01-20 08:19:55 +0200

    Add files via upload

---
+++ JavaScript/js/external.js
@@ -0,0 +1,26 @@
+function greeting() {
+  alert("Hello, Islam! from external file ");
+}
+
+      let a = 10;
+      let b = 10.25;
+      let c = "islam";
+      let d = "Mustafa";
+      let e = true;
+      let f = false;
+      let g;
+      let h = "";
+      let i = [10, 20, 30, 40, 50];
+      let j = { name: "Ahmed", age: 25, address: "Alex" };
+
+      console.log(typeof(a));
+      console.log(typeof(b));
+      console.log(typeof(c));
+      console.log(typeof(d));
+      console.log(typeof(e));
+      console.log(typeof(f));
+      console.log(typeof(g));
+      console.log(typeof(h));
+      console.log(typeof(i));
+      console.log(typeof(j));
+      console.log(typeof(k));
